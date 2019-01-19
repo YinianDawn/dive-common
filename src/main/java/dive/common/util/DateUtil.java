dc0a70a6c46352cb27a853adf75e39aded606413
dawn <dawn@yinian.tech>
2019-01-20 00:48:23 +0800

    DateUtil

---
+++ src/main/java/dive/common/util/DateUtil.java
@@ -0,0 +1,273 @@
+package dive.common.util;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 时间工具
+ *
+ * @author dawn
+ * @date 2019/1/19 23:00
+ */
+public class DateUtil {
+
+    /**
+     * 毫秒计时：秒
+     */
+    public static final long SECOND = 1000L;
+
+    /**
+     * 毫秒计时：分
+     */
+    public static final long MINUTE = SECOND * 60;
+
+    /**
+     * 毫秒计时：时
+     */
+    public static final long HOUR = MINUTE * 60;
+
+    /**
+     * 毫秒计时：天
+     */
+    public static final long DAY = HOUR * 24;
+
+    /**
+     * 毫秒计时：年
+     */
+    public static final long YEAR = DAY * 365;
+
+    /**
+     * 毫秒计时：世纪
+     */
+    public static final long CENTURY = YEAR * 100;
+
+    /**
+     * 毫秒计时：千年
+     */
+    public static final long MILLENNIUM = CENTURY * 10;
+
+    /**
+     * 日期时间格式
+     */
+    public static final DateTimeFormatter DTF_DATETIME;
+    static {
+        DTF_DATETIME = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    }
+
+    /**
+     * 日期格式
+     */
+    public static final DateTimeFormatter DTF_DATE;
+    static {
+        DTF_DATE = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    }
+
+    /**
+     * 时间格式
+     */
+    public static final DateTimeFormatter DTF_TIME;
+    static {
+        DTF_TIME = DateTimeFormatter.ofPattern("HH:mm:ss");
+    }
+    /**
+     * 标准UTC时间格式
+     */
+    public static final DateTimeFormatter DTF_ISO8601;
+    static {
+        DTF_ISO8601 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    }
+
+    /**
+     * 格式化时间
+     *
+     * @param timestamp 时间戳
+     * @param zoneId 时区
+     * @param formatter 格式
+     * @return 格式化时间
+     */
+    public static String format(Long timestamp, ZoneId zoneId, DateTimeFormatter formatter) {
+        Objects.requireNonNull(timestamp, "timestamp");
+        return LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(timestamp), zoneId)
+                .format(formatter);
+    }
+
+    /**
+     * 格式化
+     * 取系统默认时区
+     *
+     * @param date Date
+     * @param formatter 格式
+     * @return 格式化
+     */
+    public static String format(Date date, DateTimeFormatter formatter) {
+        Objects.requireNonNull(date, "date");
+        return DateUtil.format(date.getTime(), ZoneId.systemDefault(), formatter);
+    }
+
+    /**
+     * 格式化日期时间
+     * 系统默认时区
+     *
+     * @param date Date
+     * @return yyyy-MM-dd HH:mm:ss
+     */
+    public static String formatDateTime(Date date) {
+        return DateUtil.format(date, DTF_DATETIME);
+    }
+
+    /**
+     * 格式化日期
+     * 系统默认时区
+     *
+     * @param date Date
+     * @return yyyy-MM-dd
+     */
+    public static String formatDate(Date date) {
+        return format(date, DTF_DATE);
+    }
+
+    /**
+     * 格式化时间
+     * 系统默认时区
+     *
+     * @param date Date
+     * @return HH:mm:ss
+     */
+    public static String formatTime(Date date) {
+        return format(date, DTF_TIME);
+    }
+
+    /**
+     * 格式化日期时间
+     *
+     * @param timestamp 时间戳
+     * @param zoneId 时区
+     * @return yyyy-MM-dd HH:mm:ss
+     */
+    public static String formatDateTime(Long timestamp, ZoneId zoneId) {
+        return format(timestamp, zoneId, DTF_DATETIME);
+    }
+
+    /**
+     * 格式化日期
+     *
+     * @param timestamp 时间戳
+     * @param zoneId 时区
+     * @return yyyy-MM-dd
+     */
+    public static String formatDate(Long timestamp, ZoneId zoneId) {
+        return format(timestamp, zoneId, DTF_DATE);
+    }
+
+    /**
+     * 格式化时间
+     *
+     * @param timestamp 时间戳
+     * @param zoneId 时区
+     * @return HH:mm:ss
+     */
+    public static String formatTime(Long timestamp, ZoneId zoneId) {
+        return format(timestamp, zoneId, DTF_TIME);
+    }
+
+    /**
+     * 格式化日期时间
+     *
+     * @param date Date
+     * @return 2048-12-19T09:56:22.845Z
+     */
+    public static String formatISO8601(Date date) {
+        Objects.requireNonNull(date, "date");
+        return LocalDateTime
+                .ofInstant(date.toInstant(), ZoneOffset.UTC)
+                .format(DTF_ISO8601);
+    }
+
+    /**
+     * 格式化日期时间
+     *
+     * @param timestamp 时间戳
+     * @return 2048-12-19T09:56:22.845Z
+     */
+    public static String formatISO8601(Long timestamp) {
+        Objects.requireNonNull(timestamp, "timestamp");
+        return LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(timestamp), ZoneOffset.UTC)
+                .format(DTF_ISO8601);
+    }
+
+    //------------------------------------------------------------------------------
+
+    /**
+     * 格式化时间解析时间戳
+     *
+     * @param date 格式化时间
+     * @param zoneOffset 时区
+     * @param formatter 格式
+     * @return 时间戳
+     */
+    public static long parse(String date, ZoneOffset zoneOffset, DateTimeFormatter formatter) {
+        Objects.requireNonNull(date, "date");
+        return LocalDateTime
+                .parse(date, formatter)
+                .toInstant(zoneOffset)
+                .toEpochMilli();
+    }
+
+    /**
+     * 格式化时间解析时间戳
+     *
+     * @param date 格式化时间
+     * @param formatter 格式
+     * @return 时间戳
+     */
+    public static long parse(String date, DateTimeFormatter formatter) {
+        return parse(date, OffsetDateTime.now().getOffset(), formatter);
+    }
+
+    /**
+     * 格式日期时间解析时间戳
+     *
+     * @param date 格式日期时间
+     * @return 时间戳
+     */
+    public static long parseDateTime(String date) {
+        return parse(date, DTF_DATETIME);
+    }
+
+    /**
+     * 格式日期解析时间戳
+     *
+     * @param date 格式日期
+     * @return 时间戳
+     */
+    public static long parseDate(String date) {
+        return parse(date + " 00:00:00", DTF_DATETIME);
+    }
+
+    /**
+     * 格式日期时间解析时间戳
+     *
+     * @param date 格式日期时间
+     * @param zoneOffset 时区
+     * @return 时间戳
+     */
+    public static long parseDateTime(String date, ZoneOffset zoneOffset) {
+        return parse(date, zoneOffset, DTF_DATETIME);
+    }
+
+    /**
+     * 格式日期解析时间戳
+     *
+     * @param date 格式日期
+     * @param zoneOffset 时区
+     * @return 时间戳
+     */
+    public static long parseDate(String date, ZoneOffset zoneOffset) {
+        return parse(date + " 00:00:00", zoneOffset, DTF_DATETIME);
+    }
+
+}
---
+++ src/test/java/test/util/DateUtilTest.java
@@ -0,0 +1,72 @@
+package test.util;
+
+import dive.common.util.DateUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * DateUtil测试
+ *
+ * @author dawn
+ * @date 2019/1/19 23:03
+ */
+public class DateUtilTest {
+
+    @Test
+    public void test() throws ParseException {
+
+        Assert.assertEquals(1000L, DateUtil.SECOND);
+        Assert.assertEquals(1000L * 60, DateUtil.MINUTE);
+        Assert.assertEquals(1000L * 60 * 60, DateUtil.HOUR);
+        Assert.assertEquals(1000L * 60 * 60 * 24, DateUtil.DAY);
+        Assert.assertEquals(1000L * 60 * 60 * 24 * 365, DateUtil.YEAR);
+        Assert.assertEquals(1000L * 60 * 60 * 24 * 365 * 100, DateUtil.CENTURY);
+        Assert.assertEquals(1000L * 60 * 60 * 24 * 365 * 100 * 10, DateUtil.MILLENNIUM);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        // 按默认时区GMT+8进行转换时间
+        Date date = format.parse("2018-11-07 06:10:13.879");
+
+        Assert.assertEquals("2018-11-07 07:10:13", DateUtil.format(date.getTime(), ZoneId.of("+9"), DateUtil.DTF_DATETIME));
+        Assert.assertEquals("2018-11-07", DateUtil.format(date.getTime(), ZoneId.of("+9"), DateUtil.DTF_DATE));
+        Assert.assertEquals("07:10:13", DateUtil.format(date.getTime(), ZoneId.of("+9"), DateUtil.DTF_TIME));
+        Assert.assertEquals("2018-11-07T07:10:13.879Z", DateUtil.format(date.getTime(), ZoneId.of("+9"), DateUtil.DTF_ISO8601));
+        Assert.assertEquals("2018-11-07T06:10:13.879Z", DateUtil.format(date, DateUtil.DTF_ISO8601));
+
+        Assert.assertEquals("2018-11-07 06:10:13", DateUtil.formatDateTime(date));
+        Assert.assertEquals("2018-11-07", DateUtil.formatDate(date));
+        Assert.assertEquals("06:10:13", DateUtil.formatTime(date));
+
+        Assert.assertEquals("2018-11-06 14:10:13", DateUtil.formatDateTime(date.getTime(), ZoneId.of("GMT-8")));
+        Assert.assertEquals("2018-11-06", DateUtil.formatDate(date.getTime(), ZoneId.of("GMT-8")));
+        Assert.assertEquals("14:10:13", DateUtil.formatTime(date.getTime(), ZoneId.of("GMT-8")));
+
+        Assert.assertEquals("2018-11-06T22:10:13.879Z", DateUtil.formatISO8601(date));
+        Assert.assertEquals("2018-11-06T22:10:13.879Z", DateUtil.formatISO8601(date.getTime()));
+
+        //-------------------------------------
+
+        format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        // 按时区UTC进行转换时间
+        date = format.parse("2018-11-07 06:10:13.879");
+
+        Assert.assertEquals(1541571013879L, date.getTime());
+        Assert.assertEquals(1541571013879L, DateUtil.parse("2018-11-07T06:10:13.879Z", ZoneOffset.UTC, DateUtil.DTF_ISO8601));
+        Assert.assertEquals(1541571013000L, DateUtil.parse("2018-11-07 14:10:13", DateUtil.DTF_DATETIME));
+        Assert.assertEquals(1541571013000L, DateUtil.parseDateTime("2018-11-07 14:10:13"));
+        Assert.assertEquals(1541571013000L - DateUtil.HOUR * 14 - DateUtil.MINUTE * 10 - DateUtil.SECOND * 13, DateUtil.parseDate("2018-11-07"));
+
+        Assert.assertEquals(1541571013000L - DateUtil.HOUR * 8, DateUtil.parseDateTime("2018-11-07 06:10:13", ZoneOffset.ofHours(8)));
+        Assert.assertEquals(1541571013000L - DateUtil.HOUR * 6 - DateUtil.MINUTE * 10 - DateUtil.SECOND * 13 - DateUtil.HOUR * 8, DateUtil.parseDate("2018-11-07", ZoneOffset.ofHours(8)));
+
+    }
+
+}
