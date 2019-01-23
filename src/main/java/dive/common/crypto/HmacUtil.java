0683b681f572e80534f36aea43e94b57e5039bbe
dawn <dawn@yinian.tech>
2019-01-24 02:04:14 +0800

    HmacUtil

---
+++ src/main/java/dive/common/crypto/HmacUtil.java
@@ -0,0 +1,83 @@
+package dive.common.crypto;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
+import static dive.common.crypto.HexUtil.hexEncode;
+
+/**
+ * Hmac工具
+ *
+ * @author dawn
+ * @date 2019/1/19 23:00
+ */
+public class HmacUtil {
+
+    public static final String HMAC_MD5 = "HmacMD5";
+    public static final String HMAC_SHA1 = "HmacSHA1";
+    public static final String HMAC_SHA256 = "HmacSHA256";
+    public static final String HMAC_SHA384 = "HmacSHA384";
+    public static final String HMAC_SHA512 = "HmacSHA512";
+
+    public static byte[] hmac(byte[] message, String key, String algorithm)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        Objects.requireNonNull(message, "message");
+        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), algorithm);
+        Mac mac = Mac.getInstance(algorithm);
+        mac.init(signingKey);
+        return mac.doFinal(message);
+    }
+
+    private static String hmac(byte[] message, String key, String algorithm, boolean upper) {
+        try {
+            return hexEncode(hmac(message, key, algorithm), upper);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String HmacMD5(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_MD5, true);
+    }
+
+    public static String HmacSHA1(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA1, true);
+    }
+
+    public static String HmacSHA256(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA256, true);
+    }
+
+    public static String HmacSHA384(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA384, true);
+    }
+
+    public static String HmacSHA512(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA512, true);
+    }
+
+    public static String hmacMD5(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_MD5, false);
+    }
+
+    public static String hmacSHA1(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA1, false);
+    }
+
+    public static String hmacSHA256(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA256, false);
+    }
+
+    public static String hmacSHA384(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA384, false);
+    }
+
+    public static String hmacSHA512(String message, String key) {
+        return hmac(message.getBytes(), key, HMAC_SHA512, false);
+    }
+
+}
---
+++ src/test/java/test/crypto/HmacUtilTest.java
@@ -0,0 +1,26 @@
+package test.crypto;
+
+import dive.common.crypto.HmacUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * HmacUtil测试
+ *
+ * @author dawn
+ * @date 2019/1/19 22:57
+ */
+public class HmacUtilTest {
+
+    @Test
+    public void test() {
+        String message = "abc";
+        Assert.assertEquals("f71cda1c893766a115234db7fdd59f63", HmacUtil.hmacMD5(message, message));
+        Assert.assertEquals("5b333a389b4e9a2358ac5392bf2a64dc68e3c943", HmacUtil.hmacSHA1(message, message));
+        Assert.assertEquals("2f02e24ae2e1fe880399f27600afa88364e6062bf9bbe114b32fa8f23d03608a", HmacUtil.hmacSHA256(message, message));
+        Assert.assertEquals("ef38b92496d409ab82a4ebc44724b39c0be52da9805db4605958935dd74f8a16258f403b5572cce139825caec1657893", HmacUtil.hmacSHA384(message, message));
+        Assert.assertEquals("1a97e05c35e6727690dfdf2e8079b34fefabf15236abc9170dccdcf5623e4c5ce72a446842bd7607186c9e3f21c0a0edf6ab6c5ec8304a1f969c20c1455e9b7c", HmacUtil.hmacSHA512(message, message));
+
+    }
+
+}
