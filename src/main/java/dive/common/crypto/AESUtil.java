058576661ed621542c3af0fd78ca6fa6d9a163ec
dawn <dawn@yinian.tech>
2019-01-24 02:29:06 +0800

    AESUtil

---
+++ src/main/java/dive/common/crypto/AESUtil.java
@@ -0,0 +1,84 @@
+package dive.common.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
+
+import static dive.common.crypto.Base64Util.base64DecodeByString;
+import static dive.common.crypto.Base64Util.base64EncodeToString;
+import static dive.common.crypto.Util.doFinal;
+
+/**
+ * AES工具
+ *
+ * @author dawn
+ * @date 2019/1/19 23:00
+ */
+public class AESUtil {
+
+    public static final byte[] IV = "1234567812345678".getBytes();
+    public static final String PADDING = "AES/CBC/PKCS5Padding";
+
+    public static byte[] aesKey(int i) {
+        if (i == 128 || i == 192 || i == 256) {
+            try {
+                // Boucy Castle 有 PKCS7Padding 填充模式 "BC" init()
+                KeyGenerator generator = KeyGenerator.getInstance("AES");
+                generator.init(i);
+                return generator.generateKey().getEncoded();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static String aesKeyToBase64() {
+        return base64EncodeToString(aesKey(256));
+    }
+
+    private static byte[] crypt(byte[] bytes, byte[] key, byte[] iv, String padding, int mode)
+            throws GeneralSecurityException {
+        return doFinal(bytes,
+                new SecretKeySpec(key, "AES"),
+                iv, padding, mode);
+    }
+
+    public static byte[] aesEncrypt(byte[] plain, byte[] key, byte[] iv, String padding)
+            throws GeneralSecurityException {
+        return crypt(plain, key, iv, padding, Cipher.ENCRYPT_MODE);
+    }
+
+    public static byte[] aesDecrypt(byte[] cipher, byte[] key, byte[] iv, String padding)
+            throws GeneralSecurityException {
+        return crypt(cipher, key, iv, padding, Cipher.DECRYPT_MODE);
+    }
+
+    public static String aesEncrypt(String plain, String key) {
+        Objects.requireNonNull(plain, "plain");
+        Objects.requireNonNull(key, "key");
+        try {
+            byte[] bytes = aesEncrypt(plain.getBytes(), base64DecodeByString(key), IV, PADDING);
+            return base64EncodeToString(bytes);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String aesDecrypt(String cipher, String key) {
+        Objects.requireNonNull(cipher, "cipher");
+        Objects.requireNonNull(key, "key");
+        try {
+            byte[] bytes = aesDecrypt(base64DecodeByString(cipher), base64DecodeByString(key), IV, PADDING);
+            return new String(bytes);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/test/crypto/AESUtilTest.java
@@ -0,0 +1,64 @@
+package test.crypto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static dive.common.crypto.AESUtil.*;
+import static dive.common.crypto.Base64Util.base64DecodeByString;
+import static dive.common.crypto.Base64Util.base64EncodeToString;
+
+/**
+ * AESUtil测试
+ *
+ * @author dawn
+ * @date 2019/1/19 22:57
+ */
+public class AESUtilTest {
+
+    @Test
+    public void test() {
+        String key = aesKeyToBase64();
+        System.out.println("secret:");
+        System.out.println(key);
+
+        key = "1234567812345678";
+        key = base64EncodeToString(key.getBytes());
+
+        String plain = "AES";
+        System.out.println("plain:");
+        System.out.println(plain);
+
+        String cipher = aesEncrypt(plain, key);
+        System.out.println("cipher:");
+        System.out.println(cipher);
+        Assert.assertEquals("Lak2LtylufEUjD76s5P1Dg==", cipher);
+
+        String plain2 = aesDecrypt(cipher, key);
+        System.out.println("plain2:");
+        System.out.println(plain2);
+        Assert.assertEquals(plain, plain2);
+
+    }
+
+    @Test
+    public void test2() throws Exception {
+        String key = "1234567812345678";
+        String secret = base64EncodeToString(key.getBytes());
+
+        String plain = "AES";
+        System.out.println("plain:");
+        System.out.println(plain);
+
+        String cipher = base64EncodeToString(aesEncrypt(plain.getBytes(), base64DecodeByString(secret), null, "AES/ECB/PKCS5Padding"));
+        System.out.println("cipher:");
+        System.out.println(cipher);
+        Assert.assertEquals("bigEf8eBGPkkd/6NUhZ1/g==", cipher);
+
+        String plain2 = new String(aesDecrypt(base64DecodeByString(cipher), base64DecodeByString(secret), null, "AES/ECB/PKCS5Padding"));
+        System.out.println("plain2:");
+        System.out.println(plain2);
+        Assert.assertEquals(plain, plain2);
+
+    }
+
+}
