2ae2f8d9716386ade9e0ac9fa5e555a11bc289ae
dawn <dawn@yinian.tech>
2019-02-11 22:05:11 +0800

    doc

--- src/main/java/dive/common/util/Streams.java
+++ src/main/java/dive/common/util/Streams.java
@@ -11,6 +11,13 @@ import java.util.stream.Stream;
  */
 public class Streams {
 
+    /**
+     * 分割流
+     * @param stream 流
+     * @param max 最大个数
+     * @param <T> 分割基本类型
+     * @return 分割后的流
+     */
     public static <T> Stream<Stream<T>> split(Stream<T> stream, int max) {
         List<T> list = stream.collect(Collectors.toList());
         int limit = list.size() / max;
@@ -22,6 +29,13 @@ public class Streams {
                 .map(i -> list.stream().skip(i * max).limit(max));
     }
 
+    /**
+     * 分割列表
+     * @param list 列表
+     * @param max 最大个数
+     * @param <T> 分割基本类型
+     * @return 分割后的列表的列表
+     */
     public static <T> List<List<T>> split(List<T> list, int max) {
         int limit = list.size() / max;
         if (0 != list.size() % max) {
